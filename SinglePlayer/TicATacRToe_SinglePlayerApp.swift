8af3739babc523c9d543f8df0be7013a23413190
Júlio César Flores <julioflspbr@gmail.com>
2023-01-17 16:34:41 +0100

    Create workspace to hold multi and single player versions

---
+++ SinglePlayer/TicATacRToe_SinglePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicATacRToe_SinglePlayerApp.swift
+//  TicATacRToe-SinglePlayer
+//
+//  Created by Julio Flores on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicATacRToeSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
